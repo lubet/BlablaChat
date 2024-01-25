4d8d1feab101f63fe014b9a5849fb57f965fc2e4
lubet <xlubet-moncla@wanadoo.fr>
2024-01-25 11:23:38 +0100

    com23

---
+++ BlablaChat/Components/SearchBarView.swift
@@ -0,0 +1,48 @@
+//
+//  SearchBarView.swift
+//  BlablaChat
+//
+//  Created by Lubet-Moncla Xavier on 25/01/2024.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @State var searchText: String = ""
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(searchText.isEmpty ?
+                                 Color.black: Color.white
+                )
+            TextField("Recherche par le nom", text: $searchText)
+                .foregroundColor(Color.black)
+                .overlay (
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x:10)
+                        .foregroundColor(Color.black)
+                        .onTapGesture {
+                            searchText = ""
+                        }
+                    ,alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color(.init("GrisClair")))
+                .shadow(radius: 2)
+        )
+        .padding()
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView()
+    }
+}

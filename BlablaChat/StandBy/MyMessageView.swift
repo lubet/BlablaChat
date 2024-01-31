2920ff9496a85c839ac69a2bfc73b4e31dece21d
lubet <xlubet-moncla@wanadoo.fr>
2024-01-31 12:32:05 +0100

    com47

--- BlablaChat/StandBy/MyMessageView.swift
+++ BlablaChat/StandBy/MyMessageView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 @MainActor
 final class MyMessageViewModel: ObservableObject {
     
-    @Published private(set) var mesContacts: [Contact] = []
+    // init() { }
     
+    @Published private(set) var mesContacts: [Contact] = []
+
     func getContacts() async {
             self.mesContacts = await ContactManager.shared.getAllContacts()
     }
@@ -22,11 +24,18 @@ struct MyMessageView: View {
     
     @StateObject var viewModel = MyMessageViewModel()
     
+    @State private var searchText: String = ""
+    
+    var filteredContacts: [Contact] {
+        guard !searchText.isEmpty else { return viewModel.mesContacts}
+        return viewModel.mesContacts.filter { $0.nom.localizedCaseInsensitiveContains(searchText)}
+    }
+    
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack {
-                    ForEach(viewModel.mesContacts) { oneContact in
+                    ForEach(filteredContacts) { oneContact in
                         // Text(oneContact.nom)
                         ContactCellView(lecontact: oneContact)
                     }
@@ -34,9 +43,8 @@ struct MyMessageView: View {
             }
             .navigationTitle("Contacts")
         }
-        .task {
-            await viewModel.getContacts()
-        }
+        .task { await viewModel.getContacts() }
+        .searchable(text: $searchText, prompt: "Recherche d'un contact")
     }
 }
 

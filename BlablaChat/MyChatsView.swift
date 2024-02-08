c86c8ee6d4087d2d16d997e61b7f662bb5392fe6
lubet <xlubet-moncla@wanadoo.fr>
2024-02-08 10:06:08 +0100

    com69

---
+++ BlablaChat/MyChatsView.swift
@@ -0,0 +1,20 @@
+//
+//  MyChatsView.swift
+//  BlablaChat
+//
+//  Created by Lubet-Moncla Xavier on 08/02/2024.
+//
+
+import SwiftUI
+
+struct MyChatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyChatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyChatsView()
+    }
+}

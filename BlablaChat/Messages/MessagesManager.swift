e2172b8f45556b7012e1262754a25e7552c459e2
lubet <xlubet-moncla@wanadoo.fr>
2024-03-13 11:37:50 +0100

    com56

--- BlablaChat/Messages/MessagesManager.swift
+++ BlablaChat/Messages/MessagesManager.swift
@@ -11,49 +11,98 @@ import FirebaseFirestoreSwift
 
 private let db = Firestore.firestore()
 
+struct MessageItem: Identifiable, Codable {
+    let room_id: String
+    let room_name: String
+    let from_id: String
+    let to_id: String
+    let message_text: String
+    let date_send: Timestamp
+    
+    var id: String {
+       room_id
+    }
+    
+    init(
+        room_id: String,
+        room_name: String,
+        from_id: String,
+        to_id: String,
+        message_text: String,
+        date_send: Timestamp
+    ) {
+        self.room_id = room_id
+        self.room_name = room_name
+        self.from_id = from_id
+        self.to_id = to_id
+        self.message_text = message_text
+        self.date_send = Timestamp()
+    }
+}
+
+
 final class MessagesManager {
     
     static let shared = MessagesManager()
     
     init() { }
     
-    private let messageCollection = db.collection("message")
+    // -----------------------------------------------------------------
+    private let roomCollection = db.collection("rooms")
+    
+    private func roomDocument(room_id: String) -> DocumentReference {
+        return roomCollection.document(room_id)
+    }
+           
+    // Collection messages - sous collection de rooms
+    private func messageCollection(room_id: String) -> CollectionReference {
+        roomDocument(room_id: room_id).collection("messages")
+    }
+    
+    // Document message
+    private func messageDocument(room_id: String, message_id: String) -> DocumentReference {
+        messageCollection(room_id: room_id).document(message_id)
+    }
+
     
     // LastMessages
-    func getLastMessages(user_id: String) async throws -> [Message] {
+    func getLastMessages(user_id: String) async throws -> [MessageItem] {
         
-        print("getLastMessages user_id \(user_id)")
+        print("user_id:\(user_id)")
         
-        var myMessage = [Message]()
- 
-        do {
-            let messagesSnap = try await messageCollection
+        // fetch de tous les rooms où j'apparais dans le from ou dans le to
+       var myMessages = [MessageItem]()
+        
+        // Rooms
+        let roomsSnap = try await roomCollection
+            .order(by: "room_id")
+            .getDocuments()
+        
+        print("number: \(roomsSnap.documents.count)")
+        
+        for document in roomsSnap.documents {
+            let room = try document.data(as: Room.self)
+            let roomId = room.room_id
+            let roomName = room.room_name
+            
+            let messagesSnap = try await messageCollection(room_id: roomId)
                 .whereFilter(Filter.orFilter([
                     Filter.whereField("from_id", isEqualTo: user_id),
                     Filter.whereField("to_id", isEqualTo: user_id)
                 ])
                 )
-                .order(by:"room_id")
                 .order(by: "date_send")
                 .limit(to: 1)
                 .getDocuments()
-
-            print("before le for")
-
-            print("count \(messagesSnap.documents.count)")
-
-            for document in messagesSnap.documents {
-                let message = try document.data(as: Message.self)
-                myMessage.append(message)
-                print("getLastMessages from_id \(message.from_id)")
+            
+            for oneMessage in messagesSnap.documents {
+                let msg = try oneMessage.data(as: MessageItem.self)
+                myMessages.append(msg)
             }
-        } catch {
-            print("Error getting documents: \(error.localizedDescription)")
         }
-        return myMessage
+        print("getLastMessages - myRooms: \(myMessages)")
         
+        return myMessages
     }
-
-    //
     
 }

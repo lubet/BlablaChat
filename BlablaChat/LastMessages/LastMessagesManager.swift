7a259f1dd48728558579a98eb2ece8d0851e057c
lubet <xlubet-moncla@wanadoo.fr>
2024-03-24 12:17:50 +0100

    com25

--- BlablaChat/LastMessages/LastMessagesManager.swift
+++ BlablaChat/LastMessages/LastMessagesManager.swift
@@ -9,58 +9,52 @@ import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-// A mettre dans la View
-struct LastModel: Identifiable, Codable {
-    let id: String
-    let room_id: String
-    let room_name: String // nom du créateur du premier message
-    let room_date: String
-    let message_texte: String
-    let message_date: String
-    let message_from: String
-
-    enum CodingKeys: String, CodingKey {
-        case id = "id"
-        case room_id = "room_id"
-        case room_name = "room_name"
-        case room_date = "room_date"
-        case message_texte = "messge_texte"
-        case message_date = "message_date"
-        case message_from = "message_from"
-    }
-}
-
 private let db = Firestore.firestore()
 
 final class LastMessagesManager {
     
-    // -----------------------------------------------------------------
+    static let shared = LastMessagesManager()
+    
+    init() { }
+    
+    // Rooms
     private let roomCollection = db.collection("rooms")
+    
     private func roomDocument(room_id: String) -> DocumentReference {
         return roomCollection.document(room_id)
     }
 
+    // Users
     private let userCollection = db.collection("users")
+    
     private func userDocument(user_id: String) -> DocumentReference {
         return userCollection.document(user_id)
     }
     
-    // TODO - Récupérer les rooms où je suis présent
-    // room_name = email du créateur du room
-    // Avec les messages je pourrai récupérer les rooms qui correspondent à mes messages
-    func getAllRooms() async throws -> [Room] {
-        var rooms = [Room]()
+    // Membres
+    private let memberCollection = db.collection("members")
+    
+    private func memberDocument(user_id: String) -> DocumentReference {
+        return memberCollection.document(user_id)
+    }
+    
+    // Récupérer mes rooms
+    func getMyRooms(user_id: String) async throws -> [Member] {
+        var members = [Member]()
         do {
-            let querySnapshot = try await roomCollection
+            let querySnapshot = try await memberCollection.whereFilter(Filter.orFilter([
+                    Filter.whereField("from_id", isEqualTo: user_id),
+                    Filter.whereField("to_id", isEqualTo: user_id)
+                ]))
                 .order(by: "date_created")
                 .getDocuments()
             for document in querySnapshot.documents {
-                let room = try document.data(as: Room.self)
-                rooms.append(room)
+                let membre = try document.data(as: Member.self)
+                members.append(membre)
             }
         } catch {
-            print("getAllRooms - Error getting documents: \(error.localizedDescription)")
+            print("getMyRooms - Error getting documents: \(error.localizedDescription)")
         }
-        return rooms
+        return members
     }
 }
--- BlablaChat/LastMessages/LastMessagesView.swift
+++ BlablaChat/LastMessages/LastMessagesView.swift
@@ -7,43 +7,77 @@
 
 import SwiftUI
 
+// A mettre dans la View
+struct LastMessage: Identifiable, Codable {
+    let id: String
+    let room_id: String
+    let room_name: String // nom du créateur du premier message
+    let room_date: String
+    let message_texte: String
+    let message_date: String
+    let message_from: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case room_id = "room_id"
+        case room_name = "room_name"
+        case room_date = "room_date"
+        case message_texte = "messge_texte"
+        case message_date = "message_date"
+        case message_from = "message_from"
+    }
+}
+
+@MainActor
 class LastMessagesViewModel: ObservableObject {
     
-    @Published var LastArray: [LastModel] = []
+    @Published private(set) var lastMessages: [LastMessage] = []
     
-    func getLast() {
+    private var members: [Member] = []
+    
+    func getLastMessages() async {
+        
+        Task {
+            let AuthUser = try AuthManager.shared.getAuthenticatedUser()
+            let user_id = AuthUser.uid
+            
+            // members contient les rooms avec pour chaque room le dernier message
+            self.members = try await LastMessagesManager.shared.getMyRooms(user_id: user_id)
 
+            for member in members {
+                print("from: \(member.from_id), to: \(member.to_id), room: \(member.room_id)")
+            }
+            
+        }
+        
+    }
+    
+    func deleteLast(index: IndexSet) async {
+        lastMessages.remove(atOffsets: index)
     }
 
-    func deleteLast(index: IndexSet) {
-        LastArray.remove(atOffsets: index)
-    }
-
-    
 }
 
 struct LastMessagesView: View {
     
     // @ObserverObject relaod si la vue is refresh contrairement à @StateObject
-    @ObservedObject var lastViewModel: LastMessagesViewModel = LastMessagesViewModel()
+    @ObservedObject var viewModel: LastMessagesViewModel = LastMessagesViewModel()
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(lastViewModel.LastArray) { un in
+                ForEach(viewModel.lastMessages) { un in
                     HStack {
                         Text("\(un.room_name)")
                         Text("\(un.room_date)")
                         Text("\(un.message_texte)")
                     }
                 }
-                .onDelete(perform: lastViewModel.deleteLast)
+                //.onDelete(perform: viewModel.deleteLast)
             }
+            .task { await viewModel.getLastMessages() }
             // .listStyle()
             .navigationTitle("Messages")
-            .onAppear {
-                lastViewModel.getLast()
-            }
             .navigationBarItems(
                 leading: Image(systemName: "person.fill"),
                 trailing: NavigationLink(
@@ -54,6 +88,7 @@ struct LastMessagesView: View {
         }
     }
 }
+
 
 struct monProfil: View {
     var body: some View {

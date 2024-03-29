7b9f91c2a7ef12564f8782586ba9988c48dab7a8
lubet <xlubet-moncla@wanadoo.fr>
2024-03-29 16:42:31 +0100

    com89

--- BlablaChat/Messages/MessagesCellView.swift
+++ BlablaChat/Messages/MessagesCellView.swift
@@ -11,7 +11,7 @@ import FirebaseFirestoreSwift
 
 struct MessagesCellView: View {
     
-    let message: [Message]
+    let message: Message
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
@@ -20,7 +20,7 @@ struct MessagesCellView: View {
 
 struct MessagesCellView_Previews: PreviewProvider {
     static var previews: some View {
-        MessagesCellView(message: [Message(id: "123456", from_id: "123", to_id: "123", message_text: "Coucou",
-                        date_send: Timestamp(), room_id: "123")])
+        MessagesCellView(message: Message(id: "123456", from_id: "123", to_id: "123", message_text: "Coucou",
+                        date_send: Timestamp(), room_id: "123"))
     }
 }

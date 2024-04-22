f92919f930e14833780d6fed89a868b0603b58c9
lubet <xlubet-moncla@wanadoo.fr>
2024-04-22 18:33:34 +0200

    com56

--- BlablaChat/Contacts/ContactBubblesView.swift
+++ BlablaChat/Contacts/ContactBubblesView.swift
@@ -19,6 +19,8 @@ import PhotosUI
 @MainActor
 final class ContactBubblesViewModel: ObservableObject {
     
+    init() {}
+    
     @Published private(set) var messagesBubble: [MessageBubble] = []
     
     // ScrollViewReader
@@ -30,11 +32,102 @@ final class ContactBubblesViewModel: ObservableObject {
             setImage(from: imageSelection)
         }
     }
+    
+    @Published var param: [String:String] = ["":""]
+    
 
+    // Sauvegarde d'un message "Photo"
     private func setImage(from selection: PhotosPickerItem?) {
+        guard let selection else { return }
+
+        var selectedImage: UIImage? = nil
+
+        Task {
+            if let data = try? await selection.loadTransferable(type: Data.self) {
+                
+                if let uiImage = UIImage(data: data) {
+                    selectedImage = uiImage
+
+                    guard let AuthUser = try? AuthManager.shared.getAuthenticatedUser() else { return }
+                    let user_id = AuthUser.uid
+                    
+                    guard let para = param["room_id"] else {
+                        print("setImage -Erreur param")
+                        return }
+                    
+                    var room_id: String = ""
+                    
+                    if para == "" {
+                        room_id = try await ContactsManager.shared.createRoom(name: para)
+                    } else {
+                        room_id = param["room_id"] ?? ""
+                    }
+                     
+                    let toId =  try await MessagesManager.shared.getToId(room_id: room_id, user_id: user_id)
+
+                    let lurl: URL
+                    
+                    guard let image = selectedImage else { return }
+                    
+                    // path = users/user_id/<nom du fichier.jpeg
+                    let (path, _) = try await StorageManager.shared.saveImage(image: image, userId: toId)
+                    
+                    lurl = try await StorageManager.shared.getUrlForImage(path: path)
+                    
+                    try await ContactsManager.shared.createMessage(from_id: user_id, to_id: toId, message_text: "", room_id: room_id, image_link: lurl.absoluteString)
+                    
+                    do {
+                        // Rafraichissement de la view actuelle
+                        self.messagesBubble = try await MessagesManager.shared.getRoomMessages(room_id: room_id, user_id: AuthUser.uid)
+                        
+                        scrollViewReaderId()
+                        
+                    } catch {
+                        print("Error saveMessage: \(error.localizedDescription)")
+                        return
+                    }
+                    
+                }
+            }
+        }
+
+        // Sauvegarde d'un message "texte"
+        func saveMessage(message_text: String, room_id: String) async throws {
+            
+            // TODO La gestion du nouveau contact (pas de users, room, member...)
+
+            // Moi
+            guard let AuthUser = try? AuthManager.shared.getAuthenticatedUser() else { return }
+            let user_id = AuthUser.uid
+            
+            // user "destinataire" to_id à trouver dans member
+            let toId =  try await MessagesManager.shared.getToId(room_id: room_id, user_id: user_id)
+            
+            // Sauvegarde du message "texte"
+            try await ContactsManager.shared.createMessage(from_id: user_id, to_id: toId, message_text: message_text, room_id: room_id, image_link: "")
+            
+            do {
+                // Rafraichissement de la view actuelle
+                self.messagesBubble = try await MessagesManager.shared.getRoomMessages(room_id: room_id, user_id: AuthUser.uid)
+                
+                scrollViewReaderId()
+                
+            } catch {
+                print("Error saveMessage: \(error.localizedDescription)")
+                return
+            }
+            
+        }
+
+        func scrollViewReaderId() {
+            if let id = self.messagesBubble.last?.id {
+                self.lastMessageId = id
+                print("id: \(id)")
+            }
+        }
     }
     
-    // Nouveau contact ou ancien avec messages ou non
+    // Get tous les messages d'un contact existant ou rien si le contact n'existe pas encore
     func getContactMesssages(email: String) async throws {
         
         // Moi
@@ -49,12 +142,15 @@ final class ContactBubblesViewModel: ObservableObject {
             contact_id = try await ContactsManager.shared.createUser(email: email)
         }
         
-        // Recherche du couple user_id contact_id dans membre (il devrait en exister qu'une occurence)
+        // Recherche du couple user_id contact_id dans membre (il devrait en exister qu'une occurence ou pas)
         let room_id = try await ContactsManager.shared.searchDuo(user_id: user_id, contact_id: contact_id)
         
         if room_id == "" {
-            // Pas de room_id cad pas de conversation -> messagesBubble sera vide
+            // Pas encore de conversation
+            param["room_id"] = ""
         } else {
+            // Déjà une conversation (Room)
+            param["room_id"] = room_id
             self.messagesBubble = try await MessagesManager.shared.getRoomMessages(room_id: room_id, user_id: user_id)
         }
     }
@@ -96,16 +192,20 @@ struct ContactBubblesView: View {
                 }
             }
         }
+        
+        // Barre de saisie d'une message "texte" ou d'une "photo"
         MessageBar
             .alert(isPresented: $showAlert) {
                 getAlert()
             }
         .navigationTitle("Messages")
+        
+        // Chargement des messages du contact (si c'est un nouveau contact il n'y aura pas de messages).
         .task {
             do {
                 try await viewModel.getContactMesssages(email: oneContact.email) // Nouveau ou ancien contact
             } catch {
-                print("ContactBubblesView - Error getting messages: \(error.localizedDescription)")
+                print("getContactMesssages - Error getting messages: \(error.localizedDescription)")
             }
         }
     }

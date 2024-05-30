ff021cdb791bc40108f28fad3572cae5bad8dde8
lubet <xlubet-moncla@wanadoo.fr>
2024-05-30 09:29:11 +0200

    com25

--- BlablaChat/Messages/MessagesView.swift
+++ BlablaChat/Messages/MessagesView.swift
@@ -64,7 +64,7 @@ final class MessagesViewModel: ObservableObject {
                         try await UserManager.shared.updateImagePath(userId: contact_id, path: lurl.absoluteString) // maj Firestore
 
                         // créer room
-                        let room_id = try await ContactsManager.shared.createRoom(name: email, contact_id: contact_id)
+                        let room_id = try await ContactsManager.shared.createRoom(name: email)
 
                         // créer membre
                         try await ContactsManager.shared.createMembers(room_id: room_id, user_id: user_id, contact_id: contact_id)
@@ -152,7 +152,7 @@ final class MessagesViewModel: ObservableObject {
             try await UserManager.shared.updateImagePath(userId: contact_id, path: lurl.absoluteString) // maj Firestore
             
             // créer "room"
-            let room_id = try await ContactsManager.shared.createRoom(name: email, contact_id: contact_id)
+            let room_id = try await ContactsManager.shared.createRoom(name: email)
             
             // créer membre
             try await ContactsManager.shared.createMembers(room_id: room_id, user_id: user_id, contact_id: contact_id)

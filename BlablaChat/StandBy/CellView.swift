d2ca9b2be63c448ab5e44b7e2de579decaccbc73
lubet <xlubet-moncla@wanadoo.fr>
2024-06-09 11:48:36 +0200

    com56

--- BlablaChat/StandBy/CellView.swift
+++ BlablaChat/StandBy/CellView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct CellView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack(spacing: 16) {
+                Image(systemName: "person.fill")
+                    .font(.system(size: 32))
+                    .padding(8)
+                    .overlay(RoundedRectangle(cornerRadius: 44)
+                    .stroke(Color.black, lineWidth: 1))
+                    
+                VStack(alignment: .leading) {
+                    Text("email")
+                        .font(.system(size: 16, weight: .bold))
+                    Text("message")
+                        .font(.system(size: 14))
+                        .foregroundStyle(.black)
+                }
+                Spacer()
+                Text("22d")
+                    .font(.system(size: 14, weight: .semibold))
+            }
+            Divider()
+                .padding(.vertical,8)
+        }.padding(.horizontal)
     }
 }
 

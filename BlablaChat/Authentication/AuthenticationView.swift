bd0dd74b16d94217bc6e513a2c56026409b65358
lubet <xlubet-moncla@wanadoo.fr>
2024-07-26 14:31:55 +0200

    com12

--- BlablaChat/Authentication/AuthenticationView.swift
+++ BlablaChat/Authentication/AuthenticationView.swift
@@ -84,57 +84,61 @@ struct AuthenticationView: View {
     @Binding var showSignInView: Bool
     
     var body: some View {
-        VStack {
-            NavigationLink {
-                LoginEmailView(showSignInView: $showSignInView)
-            } label: {
-                Text("S'authentifié avec l'email")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(height: 45)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.bottom, 10)
-            }
-            
-            GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .dark, style: .wide, state: .normal))
-            {
-                Task {
-                    do {
-                        try await viewModel.signInGoogle()
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            VStack {
+                NavigationLink {
+                    LoginEmailView(showSignInView: $showSignInView)
+                } label: {
+                    Text("S'authentifié avec l'email")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(height: 45)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding(.bottom, 10)
+                }
+                
+                GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .dark, style: .wide, state: .normal))
+                {
+                    Task {
+                        do {
+                            try await viewModel.signInGoogle()
+                            showSignInView = false
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+                .padding(.bottom, 10)
+                
+                Button(action: {
+                    Task {
+                        do {
+                            try await viewModel.signInApple()
+                            // showSignInView = false
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }, label: {
+                    SignInWithAppleButtonViewRepresentable(type: .default, style: .black)
+                        .allowsHitTesting(false)
+                })
+                .frame(height: 55)
+                .onChange(of: viewModel.didSignInWithApple) { newValue in
+                    if newValue == true {
                         showSignInView = false
-                    } catch {
-                        print(error)
                     }
                 }
+                
+                Spacer()
             }
-            .padding(.bottom, 10)
-            
-            Button(action: {
-                Task {
-                    do {
-                        try await viewModel.signInApple()
-                        // showSignInView = false
-                    } catch {
-                        print(error)
-                    }
-                }
-            }, label: {
-                SignInWithAppleButtonViewRepresentable(type: .default, style: .black)
-                    .allowsHitTesting(false)
-            })
-            .frame(height: 55)
-            .onChange(of: viewModel.didSignInWithApple) { newValue in
-                if newValue == true {
-                    showSignInView = false
-                }
-            }
-            
-            Spacer()
+            .padding()
+            .navigationTitle("S'authentifier:")
         }
-        .padding()
-        .navigationTitle("S'authentifier:")
     }
 }
 
--- BlablaChat/LastMessages/LastMessagesCellView.swift
+++ BlablaChat/LastMessages/LastMessagesCellView.swift
@@ -15,38 +15,76 @@ struct LastMessagesCellView: View {
     
     var body: some View {
         
-        VStack {
-            HStack(spacing: 16) {
-                
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            HStack(spacing: 0) {
                 SDWebImageLoader(url: lastMessage.avatar_link, size: 40)
                 
-                // WebImage(url: URL(string: lastMessage.avatar_link))
                 VStack(alignment: .leading) {
+                    
                     let emailShort = EmailShort(email: lastMessage.email)
                     Text("\(emailShort)")
-                        .frame(width: 150,alignment: .leading)
+                        //.frame(width: 150,alignment: .leading)
+                        .foregroundColor(Color.theme.accent)
                         .font(.system(size: 16, weight: .bold))
                         .background(Color(.red))
+                        .padding(.leading, 20)
+                    
                     let msg = MessageShort(message: lastMessage.message_texte)
                     Text("\(msg)")
-                        .frame(width: 150, alignment: .leading)
+                        //.frame(width: 150, alignment: .leading)
                         .font(.system(size: 14))
                         .foregroundStyle(.black)
+                        .foregroundColor(Color.theme.accent)
                         .multilineTextAlignment(.leading)
                         .background(Color(.bleu))
+                        .padding(.leading, 20)
                 }
-                //Spacer()
-                // let myDate = timeStampToString(dateMessage: lastMessage.message_date)
-                VStack(alignment: .leading) {
-                    let myDate = dateManager(dateMessage: lastMessage.message_date)
-                    Text("\(myDate)")
-                        .frame(width: 50, alignment: .leading)
-                        .font(.system(size: 8, weight: .semibold))
-                        .background(Color(.gray))
-                }
+                Spacer()
+                
+                let myDate = dateManager(dateMessage: lastMessage.message_date)
+                Text("\(myDate)")
+                    //.frame(width: 50, alignment: .leading)
+                    .foregroundColor(Color.theme.accent)
+                    .font(.system(size: 8, weight: .semibold))
+                    .background(Color(.gray))
             }
-            .padding(.vertical,8)
-        }.padding(.horizontal)
+        }
+        
+//        VStack {
+//            HStack(spacing: 16) {
+//                
+//                SDWebImageLoader(url: lastMessage.avatar_link, size: 40)
+//                
+//                // WebImage(url: URL(string: lastMessage.avatar_link))
+//                VStack(alignment: .leading) {
+//                    let emailShort = EmailShort(email: lastMessage.email)
+//                    Text("\(emailShort)")
+//                        .frame(width: 150,alignment: .leading)
+//                        .font(.system(size: 16, weight: .bold))
+//                        .background(Color(.red))
+//                    let msg = MessageShort(message: lastMessage.message_texte)
+//                    Text("\(msg)")
+//                        .frame(width: 150, alignment: .leading)
+//                        .font(.system(size: 14))
+//                        .foregroundStyle(.black)
+//                        .multilineTextAlignment(.leading)
+//                        .background(Color(.bleu))
+//                }
+//                //Spacer()
+//                // let myDate = timeStampToString(dateMessage: lastMessage.message_date)
+//                VStack(alignment: .leading) {
+//                    let myDate = dateManager(dateMessage: lastMessage.message_date)
+//                    Text("\(myDate)")
+//                        .frame(width: 50, alignment: .leading)
+//                        .font(.system(size: 8, weight: .semibold))
+//                        .background(Color(.gray))
+//                }
+//            }
+//            .padding(.vertical,8)
+//        }
+//        .padding(.horizontal)
     }
 }
 

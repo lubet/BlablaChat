d734168b490c67280871fb0f60b46473f7fcb9aa
lubet <xlubet-moncla@wanadoo.fr>
2024-07-31 16:38:57 +0200

    com14

--- BlablaChat/Users/UsersView.swift
+++ BlablaChat/Users/UsersView.swift
@@ -14,6 +14,7 @@ import Combine
 final class UsersViewModel: ObservableObject {
     
     @Published private(set) var users: [DBUser] = []
+    @Published private(set) var usersfiltered : [DBUser] = []
     
     func loadUsers() async {
         self.users = try! await UserManager.shared.getAllUsers()
@@ -30,28 +31,36 @@ struct UsersView: View {
     let didSelectedNewUser: (String) -> () // call back
     
     @State private var searchTerm: String = ""
+        
+    var filteredUsers: [DBUser] {
+        guard !searchTerm.isEmpty else { return viewModel.users }
+        return viewModel.users.filter {$0.email!.localizedCaseInsensitiveContains(searchTerm)}
+    }
     
     var body: some View {
         NavigationStack {
             ZStack {
                 Color.theme.background
                     .ignoresSafeArea()
-                List {
-                    ForEach(viewModel.users) { oneUser in
-                        Button {
-                            presentationMode.wrappedValue.dismiss()
-                            didSelectedNewUser(oneUser.email!)
-                        } label: {
-                            UsersCellView(email: oneUser.email!)
+                VStack {
+                    List {
+                        ForEach(filteredUsers) { oneUser in
+                            Button {
+                                presentationMode.wrappedValue.dismiss()
+                                didSelectedNewUser(oneUser.email!)
+                            } label: {
+                                UsersCellView(email: oneUser.email!)
+                            }
                         }
                     }
+                    .searchable(text: $searchTerm, prompt: "Recherche")
+                    .task {
+                        await viewModel.loadUsers()
+                    }
+                    .listStyle(PlainListStyle())
                 }
-                .task {
-                    await viewModel.loadUsers()
-                }
-                .listStyle(PlainListStyle())
             }
-            .navigationTitle("Utilisateurs")
+            .navigationTitle("Participants")
             .padding(.top,10)
         }
     }

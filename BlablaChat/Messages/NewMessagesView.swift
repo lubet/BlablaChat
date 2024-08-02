2731b2fcdb3e57b39b9532c5fd07007e25d6ed7d
lubet <xlubet-moncla@wanadoo.fr>
2024-08-02 08:25:22 +0200

    com25

---
+++ BlablaChat/Messages/NewMessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  NewMessagesView.swift
+//  BlablaChat
+//
+//  Created by Lubet-Moncla Xavier on 02/08/2024.
+//
+
+import SwiftUI
+
+struct NewMessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewMessagesView()
+}
---
+++ BlablaChat/Models/NewMemberModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewMemberModel.swift
+//  BlablaChat
+//
+//  Created by Lubet-Moncla Xavier on 02/08/2024.
+//
+
+import Foundation

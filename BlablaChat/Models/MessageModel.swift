9f93d90e09e6823320c90d13616085360e937590
lubet <xlubet-moncla@wanadoo.fr>
2024-08-19 19:15:55 +0200

    com15

--- BlablaChat/Models/MessageModel.swift
+++ BlablaChat/Models/MessageModel.swift
@@ -12,7 +12,6 @@ import FirebaseFirestoreSwift
 struct Message: Identifiable, Codable {
     let id: String
     let from_id: String
-    let to_id: String
     let message_text:String
     let date_send: Timestamp
     let room_id: String
@@ -21,7 +20,6 @@ struct Message: Identifiable, Codable {
     enum CodingKeys: String, CodingKey {
         case id
         case from_id
-        case to_id
         case message_text
         case date_send
         case room_id
@@ -31,7 +29,6 @@ struct Message: Identifiable, Codable {
     init(
         id: String,
         from_id: String,
-        to_id: String,
         message_text: String,
         date_send: Timestamp,
         room_id: String,
@@ -39,7 +36,6 @@ struct Message: Identifiable, Codable {
     ) {
         self.id = id
         self.from_id = from_id
-        self.to_id = to_id
         self.message_text = message_text
         self.date_send = date_send
         self .room_id = room_id

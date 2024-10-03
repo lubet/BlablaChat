7e4a3e837fb5b3648334f651980a0a503b656b9c
lubet <xlubet-moncla@wanadoo.fr>
2024-10-03 12:17:50 +0200

    com25

--- BlablaChat/SettingsView.swift
+++ BlablaChat/SettingsView.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 @MainActor
 final class SettingsViewModel: ObservableObject {
+    
+    @Published var httpAvatar: String = ""
     
     func logOut() throws {
         try AuthManager.shared.signOut()
@@ -23,6 +26,13 @@ final class SettingsViewModel: ObservableObject {
         
         try await AuthManager.shared.resetPassword(email: email)
     }
+    
+    func getAvatar() async {
+        guard let authUser = try? AuthManager.shared.getAuthenticatedUser() else { return }
+        let user_id = authUser.uid
+        // Recherche de l'avatar dans users
+        httpAvatar = try! await UsersManager.shared.getAvatar(contact_id: user_id)
+    }
 }
 
 struct SettingsView: View {
@@ -31,37 +41,70 @@ struct SettingsView: View {
     
     @Binding var showSignInView: Bool
     
+    @State var image: UIImage?
+    
+    @State var showImagePicker: Bool = false
+    
     
     var body: some View {
-        List {
-            Button("Log out") {
-                Task {
-                    do {
-                        try viewModel.logOut()
-                        showSignInView = true
-                        return
-                    } catch {
-                        print(error)
-                    }
-                }
-            }
+        ZStack() {
             
-            Button("Reset du mot de passe") {
-                Task {
-                    do {
-                        try await viewModel.resetPassword()
-                    } catch {
-                        print(error)
+            VStack(spacing: 20) {
+                
+                Button("Log out") {
+                    Task {
+                        do {
+                            try viewModel.logOut()
+                            showSignInView = true
+                            return
+                        } catch {
+                            print(error)
+                        }
                     }
                 }
-            }
+                .padding(.top,20)
+                
+                Button("Reset du mot de passe") {
+                    Task {
+                        do {
+                            try await viewModel.resetPassword()
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+                .padding(.bottom,20)
+                
+                // Recherche l'avatar du user
+                // L'afficher
+                Button { // Avatar
+                    showImagePicker.toggle()
+                } label: {
+                    VStack {
+                        WebImage(url: URL(string: viewModel.httpAvatar))
+                            .resizable()
+                            .frame(width:120, height: 120)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.black, lineWidth: 1))
+                            .padding()
 
+//                           .overlay(RoundedRectangle(cornerRadius: 64))
+//                         .stroke(Color.black,lineWidth: 2)
+                    }
+                }
+                Spacer()
+            }
+            .onAppear {
+                Task {
+                    await viewModel.getAvatar()
+                }
+            }
         }
     }
-}
-
-struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView(showSignInView: .constant(false))
+    
+    struct SettingsView_Previews: PreviewProvider {
+        static var previews: some View {
+            SettingsView(showSignInView: .constant(false))
+        }
     }
 }

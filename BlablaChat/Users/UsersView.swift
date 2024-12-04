7d21ea3fc70090cf71e64f5390eac9f975087311
lubet <xlubet-moncla@wanadoo.fr>
2024-12-04 11:22:50 +0100

    com14

--- BlablaChat/Users/UsersView.swift
+++ BlablaChat/Users/UsersView.swift
@@ -4,122 +4,70 @@
 //
 //  Created by Lubet-Moncla Xavier on 31/07/2024.
 //
+// Affichage des users avec possibilté de recherche
+
 
 import SwiftUI
-import ContactsUI
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+@MainActor
+class UsersViewModel: ObservableObject {
+    
+    @Published private(set) var allUsers: [DBUser] = []
+    
+    func loadUsers() async throws {
+        allUsers = try await UsersManager.shared.getAllUsers()
+    }
+}
+
 
 // ---------------------
 struct UsersView: View {
     
+    @StateObject var viewModel = UsersViewModel()
+    
     @Environment(\.presentationMode) var presentationMode
-    
-    @State private var allContacts = [CNContact]()
-    
-    // Whatever the user is currently looking for
-    @State private var searchText = ""
-    
-    // Results from our existing contacts list
-    var filteredContacts: [CNContact] {
-        if searchText.isEmpty {
-            allContacts
-        } else {
-            allContacts.filter {
-                $0.givenName.localizedStandardContains(searchText)
-                || $0.familyName.localizedStandardContains(searchText)
-            }
-        }
-    }
 
     let didSelectedNewUser: (String) -> () // call back
-
+    
     var body: some View {
-        NavigationStack {
-            VStack {
-                List(filteredContacts) { contact in
-                    Text("Say hello to \(contact.givenName)")
+        
+        ZStack {
+            Color.theme.background
+            NavigationStack {
+                List {
+                    ForEach(filteredUsers) { oneUser in
                         Button {
                             presentationMode.wrappedValue.dismiss() // Fermeture de la vue
-                            didSelectedNewUser(contact.givenName)      // Ouverture de la vue précédente "NewMessagesView" avec passage de l'email selectionné
+                            didSelectedNewUser(oneUser.email!)      // Ouverture de la vue précédente "NewMessagesView" avec passage de l'email selectionné
                         } label: {
-                            // UsersCellView(oneUser: oneUser)
+                            UsersCellView(oneUser: oneUser)
                         }
-
+                    }
                 }
-                .searchable(text: $searchText)
-
-                // This will automatically show a contact if one is matched, or a Search button otherwise
-                ContactAccessButton(queryString: searchText) { results in
-                    // Run fetchContacts(with:) when a contact is selected
-                    fetchContacts(with: results)
+                .searchable(text: $searchTerm, prompt: "Recherche d'un contact")
+                .autocorrectionDisabled(true)
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+                .task {
+                    await viewModel.loadUsers() // Chargement des users de la base "Users"
                 }
-                .padding()
+                .listStyle(PlainListStyle())
+                .navigationTitle("Participants")
+                .padding(.top,10)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            Image(systemName: "xmark")
+                        }
+                    }
+                }
             }
         }
     }
-
-    
-    func fetchContacts(with identifiers: [String]) {
-        Task {
-            // Prepare the Contacts system to return the names of matching people
-            let keys = [CNContactFormatter.descriptorForRequiredKeys(for: .fullName)]
-            let fetchRequest = CNContactFetchRequest(keysToFetch: keys)
-            fetchRequest.predicate = CNContact.predicateForContacts(withIdentifiers: identifiers)
-            
-            // Store new contacts in this array
-            var newContacts = [CNContact]()
-            
-            try CNContactStore().enumerateContacts(with: fetchRequest) { contact, _ in
-                newContacts.append(contact)
-            }
-            
-            // Load is completed, so add the new contacts to our existing array
-            allContacts += newContacts
-        }
-    }
-    
-    
-    
-    
-    //        ZStack {
-    //            Color.theme.background
-    //            NavigationStack {
-    //                List {
-    //                    ForEach(filteredUsers) { oneUser in
-    //                        Button {
-    //                            presentationMode.wrappedValue.dismiss() // Fermeture de la vue
-    //                            didSelectedNewUser(oneUser.email!)      // Ouverture de la vue précédente "NewMessagesView" avec passage de l'email selectionné
-    //                        } label: {
-    //                            UsersCellView(oneUser: oneUser)
-    //                        }
-    //                    }
-    //                }
-    //                .searchable(text: $searchTerm, prompt: "Recherche d'un contact")
-    //                .autocorrectionDisabled(true)
-    //                .textInputAutocapitalization(.never)
-    //                .disableAutocorrection(true)
-    //                .task {
-    //                    await viewModel.loadUsers()
-    //                }
-    //                .listStyle(PlainListStyle())
-    //                .navigationTitle("Participants")
-    //                .padding(.top,10)
-    //                .toolbar {
-    //                    ToolbarItem(placement: .navigationBarLeading) {
-    //                        Button {
-    //                            presentationMode.wrappedValue.dismiss()
-    //                        } label: {
-    //                            Image(systemName: "xmark")
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
-    
-    //#Preview {
-    //    UsersView(didSelectedNewUser: ())
-    //}
 }
 
 //#Preview {

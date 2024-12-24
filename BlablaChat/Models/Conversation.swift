73b4fe8d16c0ec80b04048863ba94f69c184273b
lubet <xlubet-moncla@wanadoo.fr>
2024-12-24 16:50:57 +0100

    com45

--- BlablaChat/Models/Conversation.swift
+++ BlablaChat/Models/Conversation.swift
@@ -7,3 +7,12 @@
 
 import Foundation
 
+struct Conversation: Identifiable {
+    let conversation_id: String
+    let date_creation: Date = Date()
+    let last_messsage_id: String
+    
+    var id: String {
+        conversation_id
+    }
+}
---
+++ BlablaChat/Models/DBUser_Conversation.swift
@@ -0,0 +1,8 @@
+//
+//  DBUser_Conversation.swift
+//  BlablaChat
+//
+//  Created by Lubet-Moncla Xavier on 24/12/2024.
+//
+
+import Foundation

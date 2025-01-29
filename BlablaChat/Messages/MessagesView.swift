b5c9e61c4f9c0a52b517d0bd7befd5c9b8cd2321
lubet <xlubet-moncla@wanadoo.fr>
2025-01-29 12:00:36 +0100

    com48

--- BlablaChat/Messages/MessagesView.swift
+++ BlablaChat/Messages/MessagesView.swift
@@ -28,9 +28,25 @@ import PhotosUI
 final class MessagesViewModel: ObservableObject {
     
     @Published var allMessages: [Messages] = []
-    @Published private(set) var selectedImage: UIImage? = nil
-    @Published var imageSelection: PhotosPickerItem? = nil
+    @Published private(set) var selectedImage: UIImage? = nil // UI image
+    @Published var imageSelection: PhotosPickerItem? = nil  { // PhotosPicker image
+        didSet {
+            setImage(from: imageSelection)
+        }
+    }
     
+    private func setImage(from selection: PhotosPickerItem?) {
+        guard let selection else { return }
+        
+        Task {
+            if let data = try? await selection.loadTransferable(type: Data.self) {
+                if let uiImage =  UIImage(data: data) {
+                    selectedImage = uiImage
+                    return
+                }
+            }
+        }
+    }
     
     private var salonId: String = ""
     private var didAppear: Bool = false
